be51e3724fddee5f1e5de0f9e19236d7ca58bd01
Mia <121979781+MiaZhengLS@users.noreply.github.com>
2023-01-26 16:13:42 -0800

    Create draft

---
+++ WorldEditor/WorldEditorProgram.cs
@@ -0,0 +1,16 @@
+namespace WorldEditor;
+
+public class WorldEditorProgram
+{
+    private readonly HistoryManager historyManager;
+
+    public WorldEditorProgram()
+    {
+        historyManager = new HistoryManager();
+    }
+
+    public void Run()
+    {
+        // Put your tests here...
+    }
+}
